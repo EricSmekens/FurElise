cda4cb0e740d3be2cd6a61e92c397efbaaff1d71
Eric Smekens <ericsmekens@msn.com>
2013-04-06 21:18:04 +0200

    First integration of two components
    Shows current year on display now

---
+++ heartOfDays/HeartOfDays/HeartOfDays.ino
@@ -0,0 +1,327 @@
+/////*************************************** Real-time clock
+#include <Wire.h>
+#define DS1307_I2C_ADDRESS 0x68  // This is the I2C address
+// Arduino version compatibility Pre-Compiler Directives
+#if defined(ARDUINO) && ARDUINO >= 100   // Arduino v1.0 and newer
+  #define I2C_WRITE Wire.write 
+  #define I2C_READ Wire.read
+#else                                   // Arduino Prior to v1.0 
+  #define I2C_WRITE Wire.send 
+  #define I2C_READ Wire.receive
+#endif
+// Global Variables
+int command = 0;       // This is the command char, in ascii form, sent from the serial port     
+int i;
+long previousMillis = 0;        // will store last time Temp was updated
+byte second, minute, hour, dayOfWeek, dayOfMonth, month, year;
+byte test;
+byte zero;
+char  *Day[] = {"","Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
+char  *Mon[] = {"","Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
+ 
+// Convert normal decimal numbers to binary coded decimal
+byte decToBcd(byte val)
+{
+  return ( (val/10*16) + (val%10) );
+}
+ 
+// Convert binary coded decimal to normal decimal numbers
+byte bcdToDec(byte val)
+{
+  return ( (val/16*10) + (val%16) );
+}
+ 
+// 1) Sets the date and time on the ds1307
+// 2) Starts the clock
+// 3) Sets hour mode to 24 hour clock
+// Assumes you're passing in valid numbers, Probably need to put in checks for valid numbers.
+ 
+void setDateDs1307()                
+{
+ 
+   second = (byte) ((Serial.read() - 48) * 10 + (Serial.read() - 48)); // Use of (byte) type casting and ascii math to achieve result.  
+   minute = (byte) ((Serial.read() - 48) *10 +  (Serial.read() - 48));
+   hour  = (byte) ((Serial.read() - 48) *10 +  (Serial.read() - 48));
+   dayOfWeek = (byte) (Serial.read() - 48);
+   dayOfMonth = (byte) ((Serial.read() - 48) *10 +  (Serial.read() - 48));
+   month = (byte) ((Serial.read() - 48) *10 +  (Serial.read() - 48));
+   year= (byte) ((Serial.read() - 48) *10 +  (Serial.read() - 48));
+   Wire.beginTransmission(DS1307_I2C_ADDRESS);
+   I2C_WRITE(zero);
+   I2C_WRITE(decToBcd(second) & 0x7f);    // 0 to bit 7 starts the clock
+   I2C_WRITE(decToBcd(minute));
+   I2C_WRITE(decToBcd(hour));      // If you want 12 hour am/pm you need to set
+                                   // bit 6 (also need to change readDateDs1307)
+   I2C_WRITE(decToBcd(dayOfWeek));
+   I2C_WRITE(decToBcd(dayOfMonth));
+   I2C_WRITE(decToBcd(month));
+   I2C_WRITE(decToBcd(year));
+   Wire.endTransmission();
+}
+ 
+// Gets the date and time from the ds1307 and prints result
+void getDateDs1307()
+{
+  // Reset the register pointer
+  Wire.beginTransmission(DS1307_I2C_ADDRESS);
+  I2C_WRITE(zero);
+  Wire.endTransmission();
+ 
+  Wire.requestFrom(DS1307_I2C_ADDRESS, 7);
+ 
+  // A few of these need masks because certain bits are control bits
+  second     = bcdToDec(I2C_READ() & 0x7f);
+  minute     = bcdToDec(I2C_READ());
+  hour       = bcdToDec(I2C_READ() & 0x3f);  // Need to change this if 12 hour am/pm
+  dayOfWeek  = bcdToDec(I2C_READ());
+  dayOfMonth = bcdToDec(I2C_READ());
+  month      = bcdToDec(I2C_READ());
+  year       = bcdToDec(I2C_READ());
+ 
+  if (hour < 10)
+    Serial.print("0");
+  Serial.print(hour, DEC);
+  Serial.print(":");
+  if (minute < 10)
+    Serial.print("0");
+  Serial.print(minute, DEC);
+  Serial.print(":");
+  if (second < 10)
+    Serial.print("0");
+  Serial.print(second, DEC);
+  Serial.print("  ");
+  Serial.print(Day[dayOfWeek]);
+  Serial.print(", ");
+  Serial.print(dayOfMonth, DEC);
+  Serial.print(" ");
+  Serial.print(Mon[month]);
+  Serial.print(" 20");
+  if (year < 10)
+    Serial.print("0");
+  Serial.println(year, DEC);
+ 
+}
+
+///////////////////************************************* 7SEG Display
+int digit1 = 10; //PWM Display pin 1
+int digit2 = 11; //PWM Display pin 2
+int digit3 = 12; //PWM Display pin 6
+int digit4 = 13; //PWM Display pin 8
+
+int segA = 7; //Display pin 11 //Collides with serial, oh noes!
+int segB = 8; //Display pin 9 //Same!
+int segC = 2; //Display pin 12
+int segD = 3; //Display pin 3
+int segE = 4; //Display pin 5
+int segF = 5; //Display pin 14
+int segG = 6; //Display pin 10
+
+void displayNumber(int toDisplay)
+{
+  #define DISPLAY_BRIGHTNESS  300
+
+  #define DIGIT_ON  HIGH
+  #define DIGIT_OFF  LOW
+
+  long beginTime = millis();
+
+  for(int digit = 4 ; digit > 0 ; digit--) 
+  {
+    //Turn on a digit for a short amount of time
+    switch(digit) {
+    case 1:
+      digitalWrite(digit1, DIGIT_ON);
+      break;
+    case 2:
+      digitalWrite(digit2, DIGIT_ON);
+      break;
+    case 3:
+      digitalWrite(digit3, DIGIT_ON);
+      break;
+    case 4:
+      digitalWrite(digit4, DIGIT_ON);
+      break;
+    }
+
+    //Turn on the right segments for this digit
+    lightNumber(toDisplay % 10);
+    toDisplay /= 10;
+
+    delayMicroseconds(DISPLAY_BRIGHTNESS); 
+    //Display digit for fraction of a second (1us to 5000us, 500 is pretty good)
+
+    //Turn off all segments
+    lightNumber(10); 
+
+    //Turn off all digits
+    digitalWrite(digit1, DIGIT_OFF);
+    digitalWrite(digit2, DIGIT_OFF);
+    digitalWrite(digit3, DIGIT_OFF);
+    digitalWrite(digit4, DIGIT_OFF);
+  }
+
+  while( (millis() - beginTime) < 10) ; 
+  //Wait for 20ms to pass before we paint the display again
+}
+
+//Given a number, turns on those segments
+//If number == 10, then turn off number
+void lightNumber(int numberToDisplay)
+{
+
+#define SEGMENT_ON  LOW
+#define SEGMENT_OFF HIGH
+
+  switch (numberToDisplay)
+  {
+  case 0:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_ON);
+    digitalWrite(segE, SEGMENT_ON);
+    digitalWrite(segF, SEGMENT_ON);
+    digitalWrite(segG, SEGMENT_OFF);
+    break;
+
+  case 1:
+    digitalWrite(segA, SEGMENT_OFF);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_OFF);
+    digitalWrite(segE, SEGMENT_OFF);
+    digitalWrite(segF, SEGMENT_OFF);
+    digitalWrite(segG, SEGMENT_OFF);
+    break;
+
+  case 2:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_OFF);
+    digitalWrite(segD, SEGMENT_ON);
+    digitalWrite(segE, SEGMENT_ON);
+    digitalWrite(segF, SEGMENT_OFF);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+
+  case 3:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_ON);
+    digitalWrite(segE, SEGMENT_OFF);
+    digitalWrite(segF, SEGMENT_OFF);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+
+  case 4:
+    digitalWrite(segA, SEGMENT_OFF);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_OFF);
+    digitalWrite(segE, SEGMENT_OFF);
+    digitalWrite(segF, SEGMENT_ON);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+
+  case 5:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_OFF);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_ON);
+    digitalWrite(segE, SEGMENT_OFF);
+    digitalWrite(segF, SEGMENT_ON);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+
+  case 6:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_OFF);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_ON);
+    digitalWrite(segE, SEGMENT_ON);
+    digitalWrite(segF, SEGMENT_ON);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+
+  case 7:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_OFF);
+    digitalWrite(segE, SEGMENT_OFF);
+    digitalWrite(segF, SEGMENT_OFF);
+    digitalWrite(segG, SEGMENT_OFF);
+    break;
+
+  case 8:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_ON);
+    digitalWrite(segE, SEGMENT_ON);
+    digitalWrite(segF, SEGMENT_ON);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+
+  case 9:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_ON);
+    digitalWrite(segE, SEGMENT_OFF);
+    digitalWrite(segF, SEGMENT_ON);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+
+  case 10:
+    digitalWrite(segA, SEGMENT_OFF);
+    digitalWrite(segB, SEGMENT_OFF);
+    digitalWrite(segC, SEGMENT_OFF);
+    digitalWrite(segD, SEGMENT_OFF);
+    digitalWrite(segE, SEGMENT_OFF);
+    digitalWrite(segF, SEGMENT_OFF);
+    digitalWrite(segG, SEGMENT_OFF);
+    break;
+  }
+}
+
+
+///////////////////************************************** MAIN
+void setup() 
+{  
+  //7SEG
+  pinMode(segA, OUTPUT);
+  pinMode(segB, OUTPUT);
+  pinMode(segC, OUTPUT);
+  pinMode(segD, OUTPUT);
+  pinMode(segE, OUTPUT);
+  pinMode(segF, OUTPUT);
+  pinMode(segG, OUTPUT);
+
+  pinMode(digit1, OUTPUT);
+  pinMode(digit2, OUTPUT);
+  pinMode(digit3, OUTPUT);
+  pinMode(digit4, OUTPUT);
+  
+  pinMode(13, OUTPUT);
+  
+  //RTC
+  Wire.begin();
+  Serial.begin(57600); 
+  //zero=0x00;
+}
+
+int numberOfDays = 0;
+void loop()
+{
+  //Initialise something
+  getDateDs1307();
+  numberOfDays = year;
+  while(true)
+  {
+    
+    displayNumber(numberOfDays);
+    delay(10);
+  }
+}
